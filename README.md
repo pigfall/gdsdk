c622dd7ebbae605bbce223f82cb0fbc63f616cff
tzz <koi_town@outlook.com>
2022-09-06 11:11:59 +0800

    add stack

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Godot SDK gdscirpt version
+
+
+# The prefix of class: PG
+Because the gdscirpt doesn't have namespace. We use the prefix PG to namespace our class in sdk
---
+++ stack.gd
@@ -0,0 +1,18 @@
+# Represents a variable size last-in-first-out collection of elements
+class_name PG_Stack
+
+var elems = []
+
+# Insert a elem at the top of the stack.
+func push(elem):
+    self.elems.append(elem)
+
+# Removes and returns the object at the top of the stack.
+func pop():
+    self.elems.pop_back()
+
+# Returns the object at the top of stack without removing it.
+func peek():
+    self.elems.back()
+
+

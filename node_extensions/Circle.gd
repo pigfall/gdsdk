f95d69c8be290760d57c56ad3a2ebc97c9568387
pigfall <koi_town@outlook.com>
2022-10-10 13:06:56 +0800

    node extensions

---
+++ node_extensions/Circle.gd
@@ -0,0 +1,45 @@
+tool
+extends Node2D
+
+class_name PG_NodeExt_Circle
+
+export var radius =100 setget set_radius
+
+func set_radius(value):
+    radius = value
+    update()
+
+export(Color) var color = Color(0.1,0.2,0.3) setget set_color
+
+func set_color(value):
+    color =value
+    update()
+
+
+
+func draw_circle_arc_poly(center, radius, angle_from, angle_to, color):
+    var nb_points = 32
+    var points_arc = PoolVector2Array()
+    points_arc.push_back(center)
+    var colors = PoolColorArray([color])
+
+    for i in range(nb_points + 1):
+        var angle_point = deg2rad(angle_from + i * (angle_to - angle_from) / nb_points - 90)
+        points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+    draw_polygon(points_arc, colors)
+
+func _draw():
+    draw_circle(Vector2(0,0),radius,color)
+
+
+
+func draw_circle_arc(center, radius, angle_from, angle_to, color):
+    var nb_points = 32
+    var points_arc = PoolVector2Array()
+
+    for i in range(nb_points + 1):
+        var angle_point = deg2rad(angle_from + i * (angle_to-angle_from) / nb_points - 90)
+        points_arc.push_back(center + Vector2(cos(angle_point), sin(angle_point)) * radius)
+
+    for index_point in range(nb_points):
+        draw_line(points_arc[index_point], points_arc[index_point + 1], color)

e5b33d1616779ad1a76c36882eeafe8874649700
pigfall <koi_town@outlook.com>
2022-11-04 19:06:00 +0800

    SceneTree.gd

--- SceneTree.gd
+++ SceneTree.gd
@@ -5,3 +5,9 @@ static func must_find_node(nodeInSceneTree: Node,path: String):
 	assert(node!=null,"not found node by path %s" % path)
 	return node
 
+static func panic(node_in_scene_tree: Node ,msg: String):
+	var dialog = AcceptDialog.new()
+	node_in_scene_tree.add_child(dialog)
+	dialog.popup()
+	node_in_scene_tree.get_tree().paused = true
+	# TODO add callbck to dialog to quit game

e630be4d396b55801070cded2d5463ec85994e3d
pigfall <koi_town@outlook.com>
2022-11-15 18:33:31 +0800

    up

--- SceneTree.gd
+++ SceneTree.gd
@@ -13,9 +13,17 @@ static func panic(node_in_scene_tree: Node ,msg: String):
 	node_in_scene_tree.add_child(dialog)
 	dialog.popup()
 	node_in_scene_tree.get_tree().paused = true
+	assert(false,"panic: %s" % msg)
 	# TODO add callbck to dialog to quit game
 
 static func must_change_scene(node: Node,scene_path:String):
 	var err_code = node.get_tree().change_scene(scene_path)
 	if err_code != OK :
 		panic(node,"Error: change scene to %s, err code: %s" % [scene_path,err_code])
+
+static func must_connect_signal(from:Node,signal_name: String,to:Object,method_name:String):
+	var err_code = from.connect(signal_name,to,method_name)
+	if err_code != OK:
+		panic(from,"failed to connect signal")
+		return
+	
---
+++ enum.gd
@@ -0,0 +1,6 @@
+class_name PG_Enum
+
+const SIGNAL_ANIMATION_FINISHED="animation_finished"
+const SIGNAL_AREA_ENTERED="area_entered"
+
+const SIGNAL_RIGIDBODY_BODY_ENTERED="body_entered"

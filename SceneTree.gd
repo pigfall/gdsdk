66fefa865e35927baf182fc915654c9b0127afc1
pigfall <koi_town@outlook.com>
2022-11-15 23:19:49 +0800

    up

--- SceneTree.gd
+++ SceneTree.gd
@@ -21,8 +21,8 @@ static func must_change_scene(node: Node,scene_path:String):
 	if err_code != OK :
 		panic(node,"Error: change scene to %s, err code: %s" % [scene_path,err_code])
 
-static func must_connect_signal(from:Node,signal_name: String,to:Object,method_name:String):
-	var err_code = from.connect(signal_name,to,method_name)
+static func must_connect_signal(from:Node,signal_name: String,to:Object,method_name:String, binds: Array =[]):
+	var err_code = from.connect(signal_name,to,method_name,binds)
 	if err_code != OK:
 		panic(from,"failed to connect signal")
 		return

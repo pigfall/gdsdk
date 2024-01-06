36ff63337e2de12c4f3e5065ad1b26052c97e88e
pigfall <koi_town@outlook.com>
2024-01-06 15:16:33 +0800

    up

--- scenes/TouchDirectionStick/TouchDirectionStick.gd
+++ scenes/TouchDirectionStick/TouchDirectionStick.gd
@@ -3,23 +3,24 @@ extends PG_NodeExt_Circle
 
 class_name PG_TouchDirectionStick
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
 static func create()-> PG_TouchDirectionStick:
 	var scene = load("res://gdsdk/scenes/TouchDirectionStick/TouchDirectionStick.tscn")
 	return scene.instantiate()
+
+signal on_touch
+signal on_release
+signal on_drag
+signal on_double_touch
 
 var  stick:PG_NodeExt_Circle
 var is_touching:bool = false
 var touching_id =null
 var touch_time_in_msec:int=0
 
-var callback_on_touch:Callable
-var callback_on_release:Callable
-var callback_on_drag:Callable
-var callback_on_double_touch:Callable
+var callback_on_touch = null
+var callback_on_release = null
+var callback_on_drag = null
+var callback_on_double_touch = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -40,6 +41,7 @@ func handle_screen_drag_input(drag:InputEventScreenDrag):
 		else:
 			self.stick.global_position = drag.position
 			self.stick.global_position = self.global_position + (self.stick.global_position - self.global_position).limit_length(self.radius)
+		self.on_drag.emit((self.stick.global_position-self.global_position).normalized())
 		if (self.callback_on_drag != null):
 			self.callback_on_drag.call((self.stick.global_position-self.global_position).normalized())
 
@@ -51,6 +53,7 @@ func handle_screen_touch_input(touch: InputEventScreenTouch):
 				self.is_touching = false
 				self.touching_id = null
 				self.stick.position = Vector2(0,0)
+				on_release.emit()
 				if (self.callback_on_release != null ):
 					self.callback_on_release.call()
 	else:
@@ -61,8 +64,11 @@ func handle_screen_touch_input(touch: InputEventScreenTouch):
 			var prev_touch_time = self.touch_time_in_msec
 			self.touch_time_in_msec = Time.get_ticks_msec()
 			if (self.touch_time_in_msec - prev_touch_time <300):
+				on_double_touch.emit((self.stick.global_position-self.global_position).normalized())
 				if  self.callback_on_double_touch != null:
 					self.callback_on_double_touch.call((self.stick.global_position-self.global_position).normalized())
+					return 
+			on_touch.emit((self.stick.global_position-self.global_position).normalized())
 			if (self.callback_on_touch != null):
 				self.callback_on_touch.call((self.stick.global_position-self.global_position).normalized())
 
